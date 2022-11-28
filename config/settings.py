35f42400b53c6a99f86c6ccc546940e0400326f9
yesolyo <ahnyelim77@gmail.com>
2022-11-28 17:56:52 +0900

    feat: bootstrap 디자인 변경

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pybo.apps.PyboConfig',
+    'pybo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
